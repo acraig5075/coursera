075c661b23f4978e4d0ac838a6b2c0fb2439eaab
Alasdair Craig <ac@acraig.za.net>
2017-06-15 13:25:56 +0200

    In progress, week 2, assignment 5

--- algorithmic-toolbox/week2/ass5/ass5.cpp
+++ algorithmic-toolbox/week2/ass5/ass5.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <cstdint>
+#include <cassert>
 
 int64_t get_fibonacci_mod(int64_t n, int64_t m)
 {
-	if (n <= 1 || m <= 2)
+	if (n <= 1)
 		return n;
 
-	return n;
+	int64_t previous = 0;
+	int64_t current = 1;
+
+	for (int64_t i = 0; i < n - 1; ++i)
+		{
+		int64_t tmp_previous = previous;
+		previous = current;
+		current = (tmp_previous + current) % m;
+		}
+
+	return current;
 }
 
 int64_t get_pisano(int64_t m)
 {
+	if (m <= 1)
+		return m;
+
+	int64_t previous = 0;
+	int64_t current = 1;
+
+	for (int64_t i = 2; ; ++i)
+		{
+		int64_t tmp_previous = previous;
+		previous = current;
+		current = tmp_previous + current;
+
+		if (i > 2 && (previous % m == 0) && (current % m == 1))
+			return i - 1;
+		}
+
 	return 0;
 }
 
@@ -19,7 +46,9 @@ int64_t get_fibonacci_huge_mod(int64_t n, int64_t m)
 	if (n <= 1 || m <= 2)
 		return n;
 
-	return get_fibonacci_mod(n % get_pisano(m), m);
+	int64_t p = get_pisano(m);
+
+	return get_fibonacci_mod(n % p, m);
 }
 
 int main()
@@ -27,4 +56,5 @@ int main()
 	int64_t n, m;
 	std::cin >> n >> m;
 	std::cout << get_fibonacci_huge_mod(n, m) << '\n';
+	return 0;
 }

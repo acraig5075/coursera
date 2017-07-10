2ade8a9eb63ff1b7e9c68ffd75a4b2eab51c4dbe
Alasdair Craig <ac@acraig.za.net>
2017-07-10 20:38:55 +0200

    Completed week 5, assignment 3.

--- algorithmic-toolbox/week5/ass3/edit_distance.cpp
+++ algorithmic-toolbox/week5/ass3/edit_distance.cpp
@@ -1,52 +1,78 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::vector;
 using std::cout;
 
-int at(size_t r, size_t c, const vector<vector<int>> &scores)
-{
-	return scores[r][c];
-}
 
-int &at(size_t r, size_t c, vector<vector<int>> &scores)
+void output_alignment(size_t i, size_t j, const vector<vector<int>> &d, const string &a, const string &b, string &a_, string &b_)
 {
-	return scores[r][c];
+	if (i == 0 && j == 0)
+	{
+		return;
+	}
+	if (i > 0 && d[i][j] == d[i - 1][j] + 1)
+	{
+		output_alignment(i - 1, j, d, a, b, a_, b_);
+		a_ += a[i-1];
+		b_ += "-";
+	}
+	else if (j > 0 && d[i][j] == d[i][j - 1] + 1)
+	{
+		output_alignment(i, j - 1, d, a, b, a_, b_);
+		a_ += "-";
+		b_ += b[j-1];
+	}
+	else
+	{
+		output_alignment(i - 1, j - 1, d, a, b, a_, b_);
+		a_ += a[i-1];
+		b_ += b[j-1];
+	}
 }
-
-void print(const vector<vector<int>> &scores)
-{
-	for (size_t r = 0; r < scores.size(); ++r)
-		{
-		for (size_t c = 0; c < scores[r].size(); ++c)
-			{
-			cout << at(r, c, scores) << " ";
-			}
-		cout << "\n";
-		}
-	cout << "\n";
-}
-
 
 int edit_distance(const string &str1, const string &str2)
 {
 	size_t rows = str1.size() + 1;
 	size_t cols = str2.size() + 1;
 
-	vector<vector<int>> scores;
+	vector<vector<int>> distances;
 
+	// construct and initialize
 	for (size_t r = 0; r < rows; ++r)
-		scores.emplace_back(vector<int>(cols));
+		distances.emplace_back(vector<int>(cols));
 	for (size_t c = 0; c < cols; ++c)
-		at(0, c, scores) = (int)c;
+		distances[0][c] = (int)c;
 	for (size_t r = 0; r < rows; ++r)
-		at(r, 0, scores) = (int)r;
+		distances[r][0] = (int)r;
 
-	print(scores);
+	// fill
+	for (size_t j = 1; j < cols; ++j)
+	{
+		for (size_t i = 1; i < rows; ++i)
+		{
+			int insertion = distances[i]  [j-1] + 1;
+			int deletion =  distances[i-1][j]   + 1;
+			int match =     distances[i-1][j-1];
+			int mismatch =  distances[i-1][j-1] + 1;
 
-	return 0;
+			if (str1[i-1] == str2[j-1])
+				distances[i][j] = std::min({ insertion, deletion, match });
+			else
+				distances[i][j] = std::min({ insertion, deletion, mismatch });
+		}
+	}
+
+	//// optional output
+	//string outstr1, outstr2;
+	//output_alignment(rows - 1, cols - 1, distances, str1, str2, outstr1, outstr2);
+	//cout << outstr1 << "\n";
+	//cout << outstr2 << "\n";
+
+	return distances[rows-1][cols-1];
 }
 
 int main()

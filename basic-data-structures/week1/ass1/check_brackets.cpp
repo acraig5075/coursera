dc769d73f936f9335c15edc2858f6a5db65ca137
Alasdair Craig <ac@acraig.za.net>
2017-07-24 12:28:25 +0200

    Completed course 2, week 1, assignment 1.

--- basic-data-structures/week1/ass1/check_brackets.cpp
+++ basic-data-structures/week1/ass1/check_brackets.cpp
@@ -2,79 +2,77 @@
 #include <stack>
 #include <string>
 
-struct Bracket {
-    Bracket(char type, size_t position):
-        type(type),
-        position(position)
-    {}
+struct Bracket
+{
+	Bracket(char type, size_t position):
+		type(type),
+		position(position)
+	{}
 
-    bool Matchc(char c) {
-        if (type == '[' && c == ']')
-            return true;
-        if (type == '{' && c == '}')
-            return true;
-        if (type == '(' && c == ')')
-            return true;
-        return false;
-    }
-
-    char type;
+	char type;
 	size_t position;
 };
 
-int main() {
+int main()
+{
 	std::string text;
 	getline(std::cin, text);
 
 	std::stack <Bracket> obs;
+
+	auto check_expected = [&obs](char c)
+		{
+		return (!obs.empty() && obs.top().type == c);
+		};
+
 	for (size_t position = 0; position < text.length(); ++position)
-	{
+		{
 		char next = text[position];
 
 		switch (next)
-		{
-		case '(':
-		case '[':
-		case '{':
-			obs.push({ next, position + 1 });
-			break;
-		case ')':
-			if (obs.top().type != '(')
-				{
+			{
+			case '(':
+			case '[':
+			case '{':
+				obs.push({ next, position + 1 });
+				break;
+			case ')':
+				if (!check_expected('('))
+					{
 					std::cout << position + 1 << "\n";
 					return 0;
-				}
-			obs.pop();
-			break;
-		case ']':
-			if (obs.top().type != '[')
-				{
+					}
+				obs.pop();
+				break;
+			case ']':
+				if (!check_expected('['))
+					{
 					std::cout << position + 1 << "\n";
 					return 0;
-				}
-			obs.pop();
-			break;
-		case '}':
-			if (obs.top().type != '{')
-				{
+					}
+				obs.pop();
+				break;
+			case '}':
+				if (!check_expected('{'))
+					{
 					std::cout << position + 1 << "\n";
 					return 0;
-				}
-			obs.pop();
-			break;
-		default:
-			break;
+					}
+				obs.pop();
+				break;
+			default:
+				break;
+			}
 		}
-	}
 
 	if (obs.empty())
-	{
+		{
 		std::cout << "Success\n";
-	}
+		}
 	else
-	{
+		{
 		std::cout << obs.top().position << "\n";
-	}
+		}
 
 	return 0;
 }

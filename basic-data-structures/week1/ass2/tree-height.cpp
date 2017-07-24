5b90d2ae7b5a3f5588660511aad8ad8a356c6a0f
Alasdair Craig <ac@acraig.za.net>
2017-07-24 15:04:38 +0200

    Completed c2 w1 a2.

--- basic-data-structures/week1/ass2/tree-height.cpp
+++ basic-data-structures/week1/ass2/tree-height.cpp
@@ -4,6 +4,7 @@
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
+#include <algorithm>
 
 class Node;
 
@@ -26,19 +27,41 @@ public:
 	}
 };
 
-// Replace this code with a faster implementation
-int tree_height_naive(int n, const std::vector<Node> &nodes)
+int height(int node_height, const Node *node)
 {
-	int maxHeight = 0;
-	for (int leaf_index = 0; leaf_index < n; leaf_index++)
+	if (node->children.empty())
+		return node_height;
+
+	const int child_height = node_height + 1;
+
+	int max_height = child_height;
+
+	for (const auto &child : node->children)
 		{
-		int height = 0;
-		for (const Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-			height++;
-		maxHeight = std::max(maxHeight, height);
+		int val = height(child_height, child);
+
+		if (val > max_height)
+			max_height = val;
 		}
 
-	return maxHeight;
+	return max_height;
+}
+
+int tree_height(int n, const std::vector<Node> &nodes)
+{
+	auto root_itr = std::find_if(begin(nodes), end(nodes), [](const Node &node)
+		{
+		return node.parent == nullptr;
+		});
+
+	if (root_itr == end(nodes))
+		return 0;
+
+	int root_index = root_itr - begin(nodes);
+
+	int root_height = 1;
+
+	return height(root_height, &nodes[root_index]);
 }
 
 int main_with_large_stack_space()
@@ -58,7 +81,7 @@ int main_with_large_stack_space()
 		nodes[child_index].key = child_index;
 		}
 
-	int maxHeight = tree_height_naive(n, nodes);
+	int maxHeight = tree_height(n, nodes);
 
 	std::cout << maxHeight << std::endl;
 	return 0;
